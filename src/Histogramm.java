73a185d832382217c26da871a9552f439ecd3b4d
meIsCoding <v.hintzen@yahoo.de>
2016-10-18 08:45:45 +0200

    SEP-01 v1.1
    ein bisschen eleganter(?)

--- src/Histogramm.java
+++ src/Histogramm.java
@@ -1,118 +1,119 @@
-
 public class Histogramm {
 
 	public static void main (String[] args ){
-		double[] Data1 = {1.1, 1.9, 2.2, 3.0, 5.1, 5.2, 4.3, 0.1, 4.5, 5.1};
-		double[] Data2 = {8.0, 6.0, 4.0, 1.0, 2.0, 3.0, 4.0, 9.0};
-		String[] Histo1 = makeHistogramm(Data1);
-		String[] Histo2 = makeHistogramm(Data2);
-		printSquare(Histo1, Data1);
-		printSquare(Histo2, Data2);
+		String pixel 		= "X";
+		String squarePixel  = "0";
+		double[] data1 = {1.1, 1.9, 2.2, 3.0, 5.1, 5.2, 4.3, 0.1, 4.5, 5.1};
+		double[] data2 = {8.0, 6.0, 4.0, 1.0, 2.0, 3.0, 4.0, 9.0};
+		String[] histo1 = makeHistogramm(data1, pixel);
+		printSquare(histo1, data1, squarePixel);
+		String[] histo2 = makeHistogramm(data2, pixel);
+		printSquare(histo2, data2, squarePixel);
 	}
-	
-	private static String[] makeHistogramm(double[] TestArray) {
-		int MaxX = TestArray.length;
-		int MaxY = 0;
-		int[] IntArray = doubleToIntArray(TestArray);
-		for (int i =0; i < MaxX; i++){
-			if (IntArray[i] > MaxY)
-				MaxY = IntArray[i];
+
+	private static String[] makeHistogramm(double[] array, String pixel) {
+		int maxX       = array.length;
+		int maxY       = 0;
+		int[] intArray = doubleToIntArray(array);
+		for (int h =0; h < maxX; h++){
+			if (intArray[h] > maxY)
+				maxY = intArray[h];
 		}
-		String[] Lines = new String[MaxY+2];
-		for (int l = 0; l < MaxY; l++){
-			if (l==0){
-				Lines[l] = "Wert "+MaxY+"|";
-			}else if (l%2 == 0){
-				Lines[l] = "     "+(MaxY-l)+"|";
-			}else{
-				Lines[l] = "      |";
+		String[] lines = new String[maxY+2];
+		lines[0] = "Wert "+ maxY +"|";
+		for (int i = 1; i < maxY; i++){
+			lines[i] = "      ";
+			if (i%2 == 0){
+				lines[i] = lines[i].substring(0,5) + (maxY-i);
 			}
+			lines[i]+= "|";
 		}
-		for (int j = 0; j < MaxY; j++){
-			for (int k = 0; k < MaxX; k++){
-				if(IntArray[k] < MaxY-j){
-					Lines[j] = Lines[j] + " ";
+		for (int j = 0; j < maxY; j++){
+			for (int k = 0; k < maxX; k++){
+				if(intArray[k] < maxY-j){
+					lines[j] += " ";
 				}else{
-					Lines[j] = Lines[j] + "*";
+					lines[j] += pixel;
 				}
 			}
 		}
-		Lines[MaxY] = "      +";
-		for (int m = 1; m <= MaxX; m++){
+		lines[maxY] = "      +";
+		for (int m = 1; m <= maxX; m++){
 			if (m%2 != 0){
-				Lines[MaxY] = Lines[MaxY]+"-";
+				lines[maxY] += "-";
 			}else{
-				Lines[MaxY] = Lines[MaxY]+(m%10);
+				lines[maxY] += m%10;
 			}
 		}
-		Lines[MaxY+1] = "  Punkt";
-		for (int n = 0; n < MaxX; n++){
-			Lines[MaxY+1] = " "+Lines[MaxY+1];
+		lines[maxY+1] = "";
+		for (int n = 0; n < maxX; n++){
+			lines[maxY+1] += " ";
 		}
-		for(String Line : Lines){
-			System.out.println(Line);
-		}
-		System.out.println("");
-		return Lines;
+		lines[maxY+1] += "Punkt";
+		printLines(lines);
+		return lines;
 	}
-	
-	private static void printSquare(String[] Histogramm, double[] Array){
-		int[] IntArray = doubleToIntArray(Array);
-		int SquareWidth = 0;
-		int SquareHeight = 0;
-		int SquarePos = 0;
-		int SquareSize = 0;
-		for (int i = 0; i < IntArray.length; i++){
-			int WidthL = 0;
-			int WidthR =0;
-			int Height = IntArray[i];
+
+	private static void printSquare(String[] lines, double[] array, String squarePixel){
+		int[] intArray   = doubleToIntArray(array);
+		int squareWidth  = 0;
+		int squareHeight = 0;
+		int squarePos    = 0;
+		int squareSize   = 0;
+		for (int i = 0; i < intArray.length; i++){
+			int widthL = 0;
+			int widthR = 0;
+			int height = intArray[i];
 			if (i >0){
 				for (int j = 1; j <= i; j++){
-					if (IntArray[i]<= IntArray[i-j] && WidthL == j-1){
-						WidthL++;
+					if (intArray[i]<= intArray[i-j] && widthL == j-1){
+						widthL++;
 					}
 				}
 			}
-			for (int k=1; k < IntArray.length-i; k++){
-				if (IntArray[i] <= IntArray[i+k] && WidthR == k-1){
-					WidthR++;
+			for (int k=1; k < intArray.length-i; k++){
+				if (intArray[i] <= intArray[i+k] && widthR == k-1){
+					widthR++;
 				}
 			}
-			int Width = 1 +WidthL+WidthR;
-			if (Width * Height >= SquareSize){ // ">=" für rechtest-, ">" für linkestmögliches Quadrat
-				SquareSize = Width * Height;
-				SquareWidth = Width;
-				SquareHeight = Height;
-				if (WidthR==0){
-					SquarePos = i;
+			int width = 1 + widthL + widthR;
+			if (width * height >= squareSize){ // ">=" für rechtest-, ">" für linkestmögliches Quadrat
+				squareSize   = width * height;
+				squareWidth  = width;
+				squareHeight = height;
+				if (widthR==0){
+					squarePos = i;
 				}else{
-					SquarePos = i+WidthR;
+					squarePos = i+widthR;
 				}
-
 			}
 		}
-		for (int l = 0; l < Histogramm.length-2; l++){
-			if (l >Histogramm.length-SquareHeight-3){
-				String Line = Histogramm[l].substring(0, 8+(SquarePos-SquareWidth));
-				for (int m = 0; m < SquareWidth; m++){
-					Line = Line+"0";
+		for (int l = 0; l < (lines.length-2); l++){
+			if (l > (lines.length-(squareHeight+3))){
+				String line = lines[l].substring(0, 8+(squarePos-squareWidth));
+				for (int m = 0; m < squareWidth; m++){
+					line += squarePixel;
 				}
-				Line = Line + Histogramm[l].substring(8+SquarePos, Histogramm[l].length());
-				Histogramm[l] = Line;
+				line += lines[l].substring(8+squarePos, lines[l].length());
+				lines[l] = line;
 			}
 		}
-		for(String Line : Histogramm){
-			System.out.println(Line);
-		}
-		System.out.println("");
+		printLines(lines);
 	}
-	
+
 	private static int[] doubleToIntArray(double[] DoubleArray){
 		int[] IntArray = new int[DoubleArray.length];
 		for (int i = 0; i < DoubleArray.length; i++){
 			IntArray[i] = (int) Math.round(DoubleArray[i]);
 		}
 		return IntArray;
+	}
+
+	private static void printLines(String[] lines){
+		System.out.println();
+		for(String line : lines){
+			System.out.println(line);
+		}
 	}
 }
 
